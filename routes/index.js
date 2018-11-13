042ae5c91d402da8e59809e82efed38de0424d24
Jelani Woods <jmw686@nyu.edu>
2018-11-12 22:12:09 -0600

    update routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+require('../passport');
+const passport = require('passport');
 const User = require('../models/user');
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,55 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+require('../passport');
+const connect = require('connect-ensure-login');
+const passport = require('passport');
+const User = require('../models/user');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', connect.ensureLoggedIn('/users/login'), function(req, res, next) {
+  User.find({}, function(err, users) {
+   res.render('users/index', {users: users});
+  });
+});
+
+// Login
+router.get('/login', function(req, res){
+  if (req.user) {
+    res.redirect('/users/'); //+ req.user.username)
+  }
+	res.render('users/login', {user: req.user});
+});
+
+router.post('/login', function (req, res, next) {
+  passport.authenticate('local', {
+    successReturnToOrRedirect: '/users',
+    failureRedirect: '/users/login'
+  }, function(err, user, info) {
+    if (err) {console.log(err);}
+
+    res.redirect('/users');
+  })(req, res, next); //TODO this right here is the stuff I don't get
+});
+
+// logout
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/signup', function(req, res){
+	res.render('users/signup');
+});
+
+router.post('/signup', function(req, res){
+  let user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  user.save().then(result => {
+    console.log(result);
+  }).catch(err => console.log(err));
+	res.redirect('/');
 });
 
 module.exports = router;
