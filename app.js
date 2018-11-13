ad55d959aac481f58fd4f090db75e06d029c43ba
Jelani Woods <jmw686@nyu.edu>
2018-11-12 22:50:54 -0600

    fix models and packages

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
+require('dotenv').config();
+const createError = require('http-errors');
 const express = require('express'),
   passport = require('passport'),
-  session = require('express-session'),
-  createError = require('http-errors'),
-  path = require('path'),
-  cookieParser = require('cookie-parser'),
-  logger = require('morgan'),
-  indexRouter = require('./routes/index'),
-  usersRouter = require('./routes/users'),
-  app = express();
+  passportConfig = require('./passport'),
+  session = require('express-session');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+
+// mongoose.connect('mongodb://localhost:27017/mlab-test',
+//   { useNewUrlParser: true,
+//     useCreateIndex: true
+//   });
+mongoose.connect('mongodb://' + process.env.MLAB_USER + ':' + process.env.MLAB_PW + '@ds151753.mlab.com:51753/mlab-test',
+  { useNewUrlParser: true,
+    useCreateIndex: true
+  });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("this world has been connected");
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -17,7 +37,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'keyboard cat',
+   cookie: {},
+    saveUninitialized: true, resave: false
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
+app.locals.basedir = app.get('views');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -27,13 +55,6 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-app.use(session(
-  {
-    secret: 'keyboard cat',
-    cookie: {},
-    saveUninitialized: true, resave: false
-  })
-);
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
--- passport.js
+++ passport.js
@@ -1,3 +1,37 @@
 const passport = require('passport');
 const localStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
+
+passport.serializeUser(function(user, done) {
+    done(null, user._id);
+});
+
+passport.deserializeUser(function(user, done) {
+    User.findById(_id, function (err, user) {
+      done(err, user);
+    });
+});
+
+passport.use(new localStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      user.comparePassword(password, user.password, function (err, isMatch) {
+        if (err) { return done(err); }
+        if (!isMatch) { return done(null, false); }
+        return done(null, user);
+      });
+    });
+  }
+));
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/users/login');
+}
